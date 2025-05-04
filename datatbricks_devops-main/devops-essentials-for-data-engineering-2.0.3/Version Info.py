afbababeb8362a4d5916872ae232c34dd6333feb
Amzad Shaik <amzad.shaik@cloudbicep.com>
2025-05-04 12:32:36 -0500

    feat/devops

---
+++ datatbricks_devops-main/devops-essentials-for-data-engineering-2.0.3/Version Info.py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
+# MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning">
+# MAGIC </div>
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Project Information
+# MAGIC
+# MAGIC * Name: **DevOps Essentials for Data Engineering**
+# MAGIC * Version:  **2.0.3**
+# MAGIC * Built On: **April 17 2025 09:54:17**
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC &copy; 2025 Databricks, Inc. All rights reserved.<br/>
+# MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the 
+# MAGIC <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
+# MAGIC <br/><a href="https://databricks.com/privacy-policy">Privacy Policy</a> | 
+# MAGIC <a href="https://databricks.com/terms-of-use">Terms of Use</a> | 
+# MAGIC <a href="https://help.databricks.com/">Support</a>
